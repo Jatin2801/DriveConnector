434ffd4c72f92716818a8bb3b1557a98977a4889
Jatin Verma <jatin28jat@gmail.com>
2025-06-22 22:15:18 +0530

    Service-Changes

--- src/main/java/com/example/GoogleDrive/Service/GoogleDriveService.java
+++ src/main/java/com/example/GoogleDrive/Service/GoogleDriveService.java
@@ -3,26 +3,33 @@ package com.example.GoogleDrive.Service;
 import org.springframework.stereotype.Service;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import org.springframework.core.io.ClassPathResource;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.api.services.drive.Drive;
+import org.springframework.core.io.ClassPathResource;
+import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.api.services.drive.DriveScopes;
+import java.io.InputStream;
 import java.util.*;
+
 @Service
+
 public class GoogleDriveService {
 private final Drive drive;
 private final String sharedFolderId = "https://drive.google.com/drive/u/2/folders/1lqti0l96iKf7yIwXmYCFe2IAlXIbKhBh";
+
+
     public GoogleDriveService() throws Exception {
-        GoogleCredential credential = GoogleCredential.fromStream(
-                        new ClassPathResource("drive-key.json").getInputStream())
+        InputStream keyStream = new ClassPathResource("driveconnector-463715-6bdc8746d33c.json").getInputStream();
+
+        ServiceAccountCredentials credentials = (ServiceAccountCredentials) ServiceAccountCredentials.fromStream(keyStream)
                 .createScoped(Collections.singleton(DriveScopes.DRIVE));
 
         this.drive = new Drive.Builder(
                 GoogleNetHttpTransport.newTrustedTransport(),
                 JacksonFactory.getDefaultInstance(),
-                credential)
-                .setApplicationName("Spring Drive API")
+                new HttpCredentialsAdapter(credentials)
+        ).setApplicationName("Spring Drive API")
                 .build();
     }
 }
-}
+
